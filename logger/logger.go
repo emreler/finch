bae289ce8b06f27e9a617333f5334467c073a435
Emre Kayan <emrekayan@gmail.com>
2017-04-18 23:56:10 +0200

    Refactor logger

--- logger/logger.go
+++ logger/logger.go
@@ -2,33 +2,41 @@ package logger
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 )
 
 const (
 	levelInfo  = "INFO"
 	levelError = "ERROR"
+	prefix     = ""
 )
 
 // Logger .
-type Logger struct{}
+type Logger struct {
+	logger *log.Logger
+}
 
 // NewLogger returns a new Logger.
-func NewLogger() *Logger {
-	return &Logger{}
+func NewLogger(logDest io.Writer) *Logger {
+	return &Logger{logger: log.New(logDest, prefix, log.Ldate|log.Ltime)}
+}
+
+func (l *Logger) print(level string, msg string) {
+	l.logger.Printf("level=%s %s", level, msg)
 }
 
 // Info logs messages with INFO level. Parameter must be either string or JSON serializable structs.
 func (l *Logger) Info(data interface{}) {
 	if str, ok := data.(string); ok {
-		log.Printf("level=%s %s", levelInfo, str)
+		l.print(levelInfo, str)
 	} else {
 		jstring, _ := json.Marshal(data)
-		log.Printf("level=%s %s", levelInfo, jstring)
+		l.print(levelInfo, string(jstring))
 	}
 }
 
 // Error logs error with ERROR level.
 func (l *Logger) Error(err error) {
-	log.Printf("level=%s %s", levelError, err)
+	l.print(levelError, err.Error())
 }
