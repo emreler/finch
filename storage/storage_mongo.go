f66085ddd3b24eb52a373b1b1ab65f60a7c7f957
Emre Kayan <emrekayan@gmail.com>
2017-04-18 17:04:36 +0200

    Move events to storage

--- storage/storage_mongo.go
+++ storage/storage_mongo.go
@@ -14,6 +14,11 @@ import (
 	"github.com/emreler/finch/models"
 )
 
+const eventsCollection = "events"
+const typeCreateAlert = "create_alert"
+const typeCreateUser = "create_user"
+const typeProcessAlert = "process_alert"
+
 // MongoStorage struct is used for storeing persistant data of alerts
 type MongoStorage struct {
 	session *mgo.Session
@@ -130,4 +135,123 @@ func (s *MongoStorage) GetUserAlerts(userID string) ([]*models.Alert, error) {
 	}
 
 	return alerts, nil
+}
+
+// GetAlertHistory returns event history for passed alertID
+func (s *MongoStorage) GetAlertHistory(alertID string, limit int) ([]*models.ProcessAlert, error) {
+	ses := s.GetDBSession()
+	defer ses.Close()
+
+	find := struct {
+		Type  string
+		Alert bson.ObjectId
+	}{
+		typeProcessAlert,
+		bson.ObjectIdHex(alertID),
+	}
+
+	var res []*models.ProcessAlert
+
+	query := ses.DB("finch").C(eventsCollection).Find(find)
+
+	if limit != 0 && limit > 0 {
+		query = query.Limit(limit)
+	}
+
+	err := query.Sort("-createdat").All(&res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// CountProcessAlertLogs counts total number of "process_alert" events
+func (s *MongoStorage) CountProcessAlertLogs() (int, error) {
+	ses := s.GetDBSession()
+	defer ses.Close()
+
+	data := struct {
+		Type string
+	}{
+		typeProcessAlert,
+	}
+
+	count, err := ses.DB("finch").C(eventsCollection).Find(data).Count()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// LogProcessAlert creates process_alert event
+func (s *MongoStorage) LogProcessAlert(alert *models.Alert, statusCode int) error {
+	ses := s.GetDBSession()
+	defer ses.Close()
+
+	data := &models.ProcessAlert{
+		Type:       typeProcessAlert,
+		Alert:      alert.ID,
+		StatusCode: statusCode,
+		CreatedAt:  time.Now(),
+	}
+
+	err := ses.DB("finch").C(eventsCollection).Insert(data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// LogCreateAlert creates create_alert event
+func (s *MongoStorage) LogCreateAlert(alert *models.Alert) error {
+	ses := s.GetDBSession()
+	defer ses.Close()
+
+	data := struct {
+		Type      string
+		Alert     bson.ObjectId
+		CreatedAt time.Time
+	}{
+		typeCreateAlert,
+		alert.ID,
+		time.Now(),
+	}
+
+	err := ses.DB("finch").C(eventsCollection).Insert(data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// LogCreateUser creates create_user event
+func (s *MongoStorage) LogCreateUser(user *models.User) error {
+	ses := s.GetDBSession()
+	defer ses.Close()
+
+	data := struct {
+		Type      string
+		User      bson.ObjectId
+		CreatedAt time.Time
+	}{
+		typeCreateUser,
+		user.ID,
+		time.Now(),
+	}
+
+	err := ses.DB("finch").C(eventsCollection).Insert(data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
